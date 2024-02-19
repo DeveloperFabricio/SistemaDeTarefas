07561d02b4740c79ef367c995e206a400b71ec72
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2024-02-19 10:36:24 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositórios.Interfaces;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepositorio _iusuarioRepositorio;
+
+        public UsuarioController(IUsuarioRepositorio iusuarioRepositorio)
+        {
+            _iusuarioRepositorio = iusuarioRepositorio;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios() 
+        {
+            List<UsuarioModel> usuarios = await _iusuarioRepositorio.BuscarTodosUsuarios();    
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _iusuarioRepositorio.BuscarPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _iusuarioRepositorio.Adicionar(usuarioModel);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
+        {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _iusuarioRepositorio.Atualizar(usuarioModel, id);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+           bool apagado = await _iusuarioRepositorio.Apagar(id);
+            return Ok(apagado);
+        }
+
+    }
+}
---
+++ Integração/Interfaces/IViaCepIntegracao.cs
@@ -0,0 +1,9 @@
+﻿using SistemaDeTarefas.Integração.Response;
+
+namespace SistemaDeTarefas.Integração.Interfaces
+{
+    public interface IViaCepIntegracao
+    {
+        Task<ViaCepResponse> ObterDadosViaCep(string cep);
+    }
+}
